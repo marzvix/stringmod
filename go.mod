3a86705318f6bfba6430fa5776028a18e409dea0
marzvix <marzvix@gmail.com>
2023-10-06 18:31:04 -0300

    initial import

---
+++ go.mod
@@ -0,0 +1,5 @@
+module stringmod
+
+go 1.21.0
+
+require github.com/hackebrot/turtle v0.2.0 // indirect
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"stringmod/quotes"
+	"stringmod/strings"
+)
+
+func main() {
+	o, e := strings.CountOddEven("12345")
+	fmt.Println(o, e) // 3 2
+	fmt.Println(quotes.GetEmoji("turtle"))
+}
